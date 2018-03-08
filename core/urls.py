ac16d8b17e353ba7a42d25cc2f833f3fd52044d6
Vladislav Toporov <vladislavtoporov@gmail.com>
2018-03-08 21:18:31 +0300

    add user authorisation

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from core import views
+
+urlpatterns = [
+    path('sign-in/', views.sign_in, name='sign-in'),
+    path('accounts/login/', views.login_view, name='login'),
+    path('accounts/logout/', views.logout_view, name='logout'),
+    path('main/', views.main, name='main'),
+]
