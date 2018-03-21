a988e3a31b64106678a736c05c6e580818d2bb8e
Зарина Скитлс <zarina113@gmail.com>
2018-03-21 11:39:53 +0300

    + models symptom_disease

--- core/models.py
+++ core/models.py
@@ -28,8 +28,44 @@ class Disease(models.Model):
     description = models.TextField
     probability = models.DecimalField
     meds = models.ManyToManyField(Med, related_name='diseases')
-    symptoms = models.ManyToManyField(Symptom, related_name='diseases')
 
 
 class Contraindication(models.Model):
     name = models.ForeignKey(ActiveSubstance, related_name='contraindications', on_delete=models.CASCADE)
+
+
+class SpringSymptDisease(models.Model):
+	disease = models.ManyToManyField(Disease, related_name='spring_sympt_disease')
+	symptom = models.ManyToManyField(Symptom, related_name='spring_sympt_disease')
+    probability = models.DecimalField
+
+    class Meta:
+    	unique_together = ('disease', 'symptom')
+
+class WinterSymptDisease(models.Model):
+	disease = models.ManyToManyField(Disease, related_name='winter_sympt_disease')
+	symptom = models.ManyToManyField(Symptom, related_name='winter_sympt_disease')
+    probability = models.DecimalField
+
+    class Meta:
+    	unique_together = ('disease', 'symptom')
+
+class SummerSymptDisease(models.Model):
+	disease = models.ManyToManyField(Disease, related_name='summer_sympt_disease')
+	symptom = models.ManyToManyField(Symptom, related_name='summer_sympt_disease')
+    probability = models.DecimalField
+
+    class Meta:
+    	unique_together = ('disease', 'symptom')
+
+class AutumnSymptDisease(models.Model):
+	disease = models.ManyToManyField(Disease, related_name='autumn_sympt_disease')
+	symptom = models.ManyToManyField(Symptom, related_name='autumn_sympt_disease')
+    probability = models.DecimalField
+
+    class Meta:
+    	unique_together = ('disease', 'symptom')
+    		
+
+
+		
