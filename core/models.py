225bc63445c62df2540d8717f6ca47a9da0a8959
Vladislav Toporov <vladislavtoporov@gmail.com>
2018-03-25 16:34:01 +0300

    add profile model and verbose name

--- core/models.py
+++ core/models.py
@@ -1,13 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class ActiveSubstance(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "aктивное вещество"
+        verbose_name_plural = "активные вещества"
+
 
 class SideEffect(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "побочный эффект"
+        verbose_name_plural = "побочные эффекты"
 
 
 class Med(models.Model):
@@ -18,9 +35,23 @@ class Med(models.Model):
     activeSubstances = models.ManyToManyField(ActiveSubstance, related_name='meds')
     side_effects = models.ManyToManyField(SideEffect, related_name='meds')
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "препарат"
+        verbose_name_plural = "препараты"
+
 
 class Symptom(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "симптом"
+        verbose_name_plural = "симптомы"
 
 
 class Disease(models.Model):
@@ -29,9 +60,23 @@ class Disease(models.Model):
     probability = models.DecimalField
     meds = models.ManyToManyField(Med, related_name='diseases')
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "заболевание"
+        verbose_name_plural = "заболевания"
+
 
 class Contraindication(models.Model):
     name = models.ForeignKey(ActiveSubstance, related_name='contraindications', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "противопоказание"
+        verbose_name_plural = "противопоказания"
 
 
 class SpringSymptDisease(models.Model):
@@ -42,6 +87,7 @@ class SpringSymptDisease(models.Model):
     # class Meta:
     #     unique_together = ('disease', 'symptom')
 
+
 class WinterSymptDisease(models.Model):
     disease = models.ManyToManyField(Disease, related_name='winter_sympt_disease')
     symptom = models.ManyToManyField(Symptom, related_name='winter_sympt_disease')
@@ -49,6 +95,7 @@ class WinterSymptDisease(models.Model):
 
     # class Meta:
     #     unique_together = ('disease', 'symptom')
+
 
 class SummerSymptDisease(models.Model):
     disease = models.ManyToManyField(Disease, related_name='summer_sympt_disease')
@@ -58,6 +105,7 @@ class SummerSymptDisease(models.Model):
     # class Meta:
     #     unique_together = ('disease', 'symptom')
 
+
 class AutumnSymptDisease(models.Model):
     disease = models.ManyToManyField(Disease, related_name='autumn_sympt_disease')
     symptom = models.ManyToManyField(Symptom, related_name='autumn_sympt_disease')
@@ -65,3 +113,27 @@ class AutumnSymptDisease(models.Model):
 
     # class Meta:
     #     unique_together = ('disease', 'symptom')
+
+class Specialization(models.Model):
+    SPEC_CHOICES = (
+        ('0', 'therapist'),
+    )
+    student_type = models.CharField(max_length=2, choices=SPEC_CHOICES)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=50, blank=False)
+    organisation = models.CharField(max_length=50, blank=False)
+    specialization = models.ManyToManyField(to=Specialization, related_name="specializations")
+    is_chief = models.BooleanField(default=False)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
