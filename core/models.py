b64eaf0edb77011bba00e75da8923c41b0914cbe
Vladislav Toporov <vladislavtoporov@gmail.com>
2018-03-25 17:14:15 +0300

    fix some models

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ from django.dispatch import receiver
 
 
 class ActiveSubstance(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="название")
 
     def __str__(self):
         return self.name
@@ -16,8 +16,8 @@ class ActiveSubstance(models.Model):
 
 
 class SideEffect(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField
+    name = models.CharField(max_length=100, verbose_name="название")
+    description = models.TextField(verbose_name="описание", blank=True)
 
     def __str__(self):
         return self.name
@@ -28,12 +28,12 @@ class SideEffect(models.Model):
 
 
 class Med(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField
-    category = models.CharField(max_length=100)
-    efficiency_class = models.IntegerField
-    activeSubstances = models.ManyToManyField(ActiveSubstance, related_name='meds')
-    side_effects = models.ManyToManyField(SideEffect, related_name='meds')
+    name = models.CharField(max_length=100, verbose_name="название")
+    description = models.TextField(verbose_name="описание", blank=True)
+    category = models.CharField(max_length=100, blank=True, verbose_name="класс принадлежности")
+    efficiency_class = models.IntegerField(null=True, blank=True, verbose_name="класс эффективности")
+    activeSubstances = models.ManyToManyField(ActiveSubstance, related_name='meds', verbose_name='активные вещества')
+    side_effects = models.ManyToManyField(SideEffect, related_name='meds', verbose_name='побочные эффекты')
 
     def __str__(self):
         return self.name
@@ -44,7 +44,7 @@ class Med(models.Model):
 
 
 class Symptom(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name='название')
 
     def __str__(self):
         return self.name
@@ -55,10 +55,10 @@ class Symptom(models.Model):
 
 
 class Disease(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField
-    probability = models.DecimalField
-    meds = models.ManyToManyField(Med, related_name='diseases')
+    name = models.CharField(max_length=100, verbose_name='название')
+    description = models.TextField(verbose_name='описание', blank=True)
+    counter = models.IntegerField(default=0)
+    meds = models.ManyToManyField(Med, related_name='diseases', verbose_name='используемые препараты')
 
     def __str__(self):
         return self.name
@@ -82,7 +82,7 @@ class Contraindication(models.Model):
 class SpringSymptDisease(models.Model):
     disease = models.ManyToManyField(Disease, related_name='spring_sympt_disease')
     symptom = models.ManyToManyField(Symptom, related_name='spring_sympt_disease')
-    probability = models.DecimalField
+    counter = models.IntegerField(default=0)
 
     # class Meta:
     #     unique_together = ('disease', 'symptom')
@@ -91,7 +91,7 @@ class SpringSymptDisease(models.Model):
 class WinterSymptDisease(models.Model):
     disease = models.ManyToManyField(Disease, related_name='winter_sympt_disease')
     symptom = models.ManyToManyField(Symptom, related_name='winter_sympt_disease')
-    probability = models.DecimalField
+    counter = models.IntegerField(default=0)
 
     # class Meta:
     #     unique_together = ('disease', 'symptom')
@@ -100,7 +100,7 @@ class WinterSymptDisease(models.Model):
 class SummerSymptDisease(models.Model):
     disease = models.ManyToManyField(Disease, related_name='summer_sympt_disease')
     symptom = models.ManyToManyField(Symptom, related_name='summer_sympt_disease')
-    probability = models.DecimalField
+    counter = models.IntegerField(default=0)
 
     # class Meta:
     #     unique_together = ('disease', 'symptom')
@@ -109,7 +109,7 @@ class SummerSymptDisease(models.Model):
 class AutumnSymptDisease(models.Model):
     disease = models.ManyToManyField(Disease, related_name='autumn_sympt_disease')
     symptom = models.ManyToManyField(Symptom, related_name='autumn_sympt_disease')
-    probability = models.DecimalField
+    counter = models.IntegerField(default=0)
 
     # class Meta:
     #     unique_together = ('disease', 'symptom')
@@ -118,15 +118,21 @@ class Specialization(models.Model):
     SPEC_CHOICES = (
         ('0', 'therapist'),
     )
-    student_type = models.CharField(max_length=2, choices=SPEC_CHOICES)
+    spec_type = models.CharField(max_length=2, choices=SPEC_CHOICES)
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.CharField(max_length=50, blank=False)
-    organisation = models.CharField(max_length=50, blank=False)
-    specialization = models.ManyToManyField(to=Specialization, related_name="specializations")
-    is_chief = models.BooleanField(default=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='пользователь')
+    bio = models.CharField(max_length=50, blank=True)
+    organisation = models.CharField(max_length=50, blank=False, verbose_name='место работы')
+    specialization = models.ManyToManyField(to=Specialization, related_name="specializations", verbose_name='специализации')
+    is_chief = models.BooleanField(default=False, verbose_name='должность главного врача')
 
+    def __str__(self):
+        return self.user.get_full_name()
+
+    class Meta:
+        verbose_name = 'профиль'
+        verbose_name_plural = 'профили'
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
