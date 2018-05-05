aee6f72d8426046edf3ef40fdd3b9a7aa42daa07
makarovTim <timurmakarov@fosslabs.ru>
2018-05-06 02:29:15 +0300

    setting improved

--- HouseMD/settings.py
+++ HouseMD/settings.py
@@ -48,6 +48,12 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
